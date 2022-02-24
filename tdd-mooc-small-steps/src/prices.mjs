ebe59646790bfbc948993db0f029dfd438090dba
Kimi <porthan.kimi@gmail.com>
2022-02-24 23:46:14 +0200

    give isHoliday only date2

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -77,7 +77,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date, date2)) {
+    if (date && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
@@ -87,12 +87,11 @@ function createApp(database) {
     return date2.dayOfWeek === 1;
   }
 
-  function isHoliday(date, date2) {
+  function isHoliday(date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
-        date &&
         date2 &&
         date2.year === holiday2.year &&
         date2.month === holiday2.month &&
