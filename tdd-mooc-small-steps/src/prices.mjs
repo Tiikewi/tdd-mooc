522fe4d6a8f0c062804600272ab8f87812013308
Kimi <porthan.kimi@gmail.com>
2022-02-24 22:16:58 +0200

    ismonday2

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -85,6 +85,9 @@ function createApp(database) {
   function isMonday(date) {
     return date.getDay() === 1;
   }
+  function isMonday2(date2) {
+    return date2.dayOfWeek === 1;
+  }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
