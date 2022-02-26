ff5e6d9197d3c6bea2d4b83306c586f3eaa66970
Kimi <porthan.kimi@gmail.com>
2022-02-26 17:25:34 +0200

    when dropped block in middle top row

--- tetris/src/Board.mjs
+++ tetris/src/Board.mjs
@@ -5,18 +5,27 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.falling = false;
   }
 
   toString() {
     const BOARD_MARKER = ".";
     let string = "";
 
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        string += BOARD_MARKER;
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        if (col === 1 && row === 0 && this.falling) {
+          string += "X";
+        } else {
+          string += BOARD_MARKER;
+        }
       }
       string += "\n";
     }
     return string;
+  }
+
+  drop() {
+    this.falling = true;
   }
 }
