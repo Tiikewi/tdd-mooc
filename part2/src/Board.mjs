c7bbc08f73c8ca9118624ce8817c0f68c6b51431
Kimi <porthan.kimi@gmail.com>
2023-02-06 23:56:56 +0200

    refactor board init

--- part2/src/Board.mjs
+++ part2/src/Board.mjs
@@ -3,20 +3,28 @@ const EMPTY_MARK = '.'
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    
+    this.initBoard()
+  }
+
+  initBoard() {
+    let row = Array(this.width).fill(EMPTY_MARK)
+    row.push('\n')
+    
+    // use join so there are no commas
+    this.board = Array(this.height).fill(row.join(""))
   }
 
   toString() {
-    let row = Array(this.width).fill(EMPTY_MARK)
-    row.push('\n')
+    return this.board.join("");
+  }
 
-    // use join so there are no commas
-    let board = Array(this.height).fill(row.join("")).join("")
-
-    return board;
-
+  drop(block) {
+    
   }
 }
