3c0b4d6d5be54c79934244b5be58c9c7bfaa20ba
Kimi <porthan.kimi@gmail.com>
2023-02-07 00:42:39 +0200

    prevent multuole falling block at the same time

--- part2/src/Board.mjs
+++ part2/src/Board.mjs
@@ -1,40 +1,47 @@
-const EMPTY_MARK = '.'
+const EMPTY_MARK = ".";
 
 export class Board {
   width;
   height;
   board;
+  isFalling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    
-    this.initBoard()
+    this.isFalling = false;
+
+    this.initBoard();
   }
 
   initBoard() {
     // init board with rows with new reference.
-    this.board = Array.from({length: this.height}, _ => (new Array(this.width).fill(EMPTY_MARK)))
+    this.board = Array.from({ length: this.height }, (_) =>
+      new Array(this.width).fill(EMPTY_MARK)
+    );
   }
 
   toString() {
-    let stringBoard = []
+    let stringBoard = [];
 
-    this.board.forEach(row => {
-      const row2 = row.concat('\n')
-      stringBoard.push(row2.join(""))
+    this.board.forEach((row) => {
+      const row2 = row.concat("\n");
+      stringBoard.push(row2.join(""));
     });
 
-    return stringBoard.join("")
+    return stringBoard.join("");
   }
 
   drop(block) {
-    this.board[0][Math.floor(this.width / 2)] = block.color
+    if (this.isFalling === true) throw new Error("already falling")
+    
+    this.isFalling = true
+    this.board[0][Math.floor(this.width / 2)] = block.color;
   }
 
   tick() {
-    this.board.pop()
-    const row = new Array(this.width).fill(EMPTY_MARK)
-    this.board.unshift(row)
+    this.board.pop();
+    const row = new Array(this.width).fill(EMPTY_MARK);
+    this.board.unshift(row);
   }
 }
