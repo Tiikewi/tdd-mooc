aa23bf58d017bcc692ccb7d6a15af726df5c9d0a
Kimi <porthan.kimi@gmail.com>
2023-02-06 21:19:03 +0200

    month === hoiliday

--- src/prices.mjs
+++ src/prices.mjs
@@ -90,12 +90,11 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
-      console.log(date2.year, holiday.getFullYear())
       if (
         date &&
         date2 &&
         date2.year === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
+        date2.month - 1 === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
         return true;
